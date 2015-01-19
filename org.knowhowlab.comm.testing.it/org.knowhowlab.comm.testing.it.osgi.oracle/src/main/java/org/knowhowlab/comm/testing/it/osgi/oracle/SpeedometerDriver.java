d7444b0cfe7de6f124edafc1c2b9b18ae2f88e16
DmytroPishchukhin <dmytro.pishchukhin@gmail.com>
2015-01-19 16:09:31 +0100

    add empty OSGi test bundle

---
+++ org.knowhowlab.comm.testing.it/org.knowhowlab.comm.testing.it.osgi.oracle/src/main/java/org/knowhowlab/comm/testing/it/osgi/oracle/SpeedometerDriver.java
@@ -0,0 +1,16 @@
+package org.knowhowlab.comm.testing.it.osgi.oracle;
+
+import org.osgi.service.component.ComponentContext;
+
+/**
+ * @author dpishchukhin
+ */
+public class SpeedometerDriver {
+    protected void activate(ComponentContext context) {
+
+    }
+
+    protected void deactivate(ComponentContext context) {
+
+    }
+}
